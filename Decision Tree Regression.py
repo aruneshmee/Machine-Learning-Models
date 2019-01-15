57b8792fa66c62fd1e0d2278ed18f48674fe1133
Arunesh Mishra <41530378+aruneshmee@users.noreply.github.com>
2019-01-14 22:33:54 -0500

    Update Decision Tree Regression.py

--- Decision Tree Regression.py
+++ Decision Tree Regression.py
@@ -1,7 +1,6 @@
 # Decision Tree Regression
 
 # Importing the libraries
-import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -11,8 +10,8 @@ X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
 # Splitting the dataset into the Training set and Test set
-"""from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
